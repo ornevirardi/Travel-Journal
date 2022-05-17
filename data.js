8fd649a9662b637bfbf3c5538d2dc1eca7f76123
Orne Virardi <73016255+ornevirardi@users.noreply.github.com>
2022-05-17 10:28:19 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,43 @@
+export default [
+    {
+        id: 1,
+        title: "San Francisco",
+        location: "California",
+        googleMapsUrl: "https://www.google.com/maps/place/San+Francisco,+CA/@37.7576793,-122.50764,12z/data=!3m1!4b1!4m5!3m4!1s0x80859a6d00690021:0x4a501367f076adff!8m2!3d37.7749295!4d-122.4194155",
+        startDate: "06 Mar, 2020",
+        endDate: "Never Ending",
+        description: "San Francisco is a cultural, commercial, and financial center in the U.S. state of California. Located in Northern California, San Francisco is the 17th most populous city proper in the United States, and the fourth most populous in California, with 873,965 residents as of 2020.",
+        imageUrl: "https://www.mymove.com/wp-content/uploads/2020/05/GettyImages-923244752-scaled.jpg"
+    },
+        {
+        id: 2,
+        title: "Santa Barbara",
+        location: "California",
+        googleMapsUrl: "https://www.google.com/maps?q=Santa+barbara+california&source=lmns&bih=778&biw=1536&client=firefox-b-1-d&hl=en&sa=X&ved=2ahUKEwiW4vKVlOX3AhWbBDQIHQowD4MQ_AUoAXoECAEQAQ",
+        startDate: "03 May, 2020",
+        endDate: "15 May, 2022",
+        description: "Santa Barbara is a city on the central California coast, with the Santa Ynez Mountains as dramatic backdrop. Downtown, Mediterranean-style white stucco buildings with red-tile roofs reflect the city’s Spanish colonial heritage. Upscale boutiques and restaurants offering local wines and seasonal fare line State Street. On a nearby hill, Mission Santa Barbara, founded in 1786, houses Franciscan friars and a museum.",
+        imageUrl: "https://blog-www.pods.com/wp-content/uploads/2021/04/resized_FI_Getty_Santa-Barbara-CA.jpg"
+    },
+        {
+        id: 3,
+        title: "Los Angeles",
+        location: "California",
+        googleMapsUrl: "https://www.google.com/maps/place/Los+Angeles,+CA/@34.0201613,-118.69192,10z/data=!3m1!4b1!4m5!3m4!1s0x80c2c75ddc27da13:0xe22fdf6f254608f4!8m2!3d34.0522342!4d-118.2436849?hl=en",
+        startDate: "06 Sep, 2020",
+        endDate: "16 Sep, 2021",
+        description: "Los Angeles is a sprawling Southern California city and the center of the nation’s film and television industry. Near its iconic Hollywood sign, studios such as Paramount Pictures, Universal and Warner Brothers offer behind-the-scenes tours. On Hollywood Boulevard, TCL Chinese Theatre displays celebrities’ hand- and footprints, the Walk of Fame honors thousands of luminaries and vendors sell maps to stars’ homes.",
+        imageUrl: "https://i.natgeofe.com/n/f7145fb3-d6ad-41bb-8065-1d7312e72bf4/hollywood-boulevard-los-angeles-california.jpg"
+    },
+        {
+        id: 4,
+        title: "Atlanta",
+        location: "Georgia",
+        googleMapsUrl: "https://www.google.com/maps/place/Atlanta,+GA/data=!4m2!3m1!1s0x88f5045d6993098d:0x66fede2f990b630b?sa=X&ved=2ahUKEwj0g8LYlOX3AhW4I0QIHUX5DF0Q8gF6BQiFARAB",
+        startDate: "27 Mar, 2022",
+        endDate: "03 Apr, 2022",
+        description: "Atlanta is the capital of the U.S. state of Georgia. It played an important part in both the Civil War and the 1960s Civil Rights Movement. Atlanta History Center chronicles the city's past, and the Martin Luther King Jr. National Historic Site is dedicated to the African-American leader’s life and times. Downtown, Centennial Olympic Park, built for the 1996 Olympics, encompasses the massive Georgia Aquarium.",
+        imageUrl: "http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcTw83ZXM_HpG8MyHyj0QXvewqoLWkb40Wtoa1xS1wo7OtEU-4V3L6RGvfj-4amh"
+    },
+
+]
